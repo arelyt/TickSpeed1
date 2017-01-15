1435c925270ca2139a5ed63072fdd91fab25ff7d
arelyt <arelyt26@gmail.com>
2017-01-15 23:57:25 +0300

    Diff

---
+++ TickSpeed/Differ.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using TSLab.Script.Handlers;
+
+namespace TickSpeed
+{
+    // Дифференциал
+    [HandlerCategory("Arelyt")]
+    [HandlerName("Diff")]
+    public class DifferClass : IDouble2DoubleHandler, IValuesHandlerWithNumber
+    {
+        public IList<double> Execute(IList<double> myDoubles)
+        {
+            var count = myDoubles.Count;
+            var values = new double[count];
+            values[0] = 0;
+            for (var i = 1; i <= count; i++)
+            {
+                values[i] = values[i] - values[i - 1];
+            }
+            return values;
+        }
+        
+    }
+
+}
