f8b1a6e98762c0e20e4fcfba3f4a3621e97eb21a
arelyt <arelyt26@gmail.com>
2017-04-01 15:57:24 +0300

    delta

---
+++ TickSpeed/DeltaCandle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using TSLab.DataSource;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace TickSpeed
+{
+    /* 
+    Аналог сжатия ТСлаба, но по шагам дельты для дальнейшей возможной группировки в зависимости от частотного анализа рынка.
+    */
+
+    [HandlerCategory("Arelyt")]
+    [HandlerName("Delta Candles")]
+    public class DeltaCandle : IOneSourceHandler, ISecurityInputs, ISecurityReturns, IStreamHandler, IContextUses
+    {
+        public IContext Context { set; get; }
+
+        [HandlerParameter(Name = "Ширина свечи(шагов дельты)", Default = "500", Min = "50", Max = "2000", Step = "10")]
+        public int Step { get; set; }
+
+        //        [HandlerParameter(Name = "Полный расчет", Default = "false", NotOptimized = true)]
+        //        public bool CalcFullCandle { get; set; }
+
+        public ISecurity Execute(ISecurity sec)
+        {
+            if (sec.IntervalBase.ToString() != "TICKS" || sec.Interval.ToString() != "1")
+                throw new Exception("Base Interval wrong. Please set to Ticks 1");
+
+            //var cache = ctx.LoadObject("TickCandle");
+            //if (cache==null)
+            //{
+                
+
+            //} 
+            // Главный цикл по тикам с шагом Step
+            var tickcount = sec.Bars.Count;
+            var values = new double[tickcount];
+            for (var i = 0; i < tickcount; i += Step)
+            {
+                // Проверка на последнюю свечу
+                if ((tickcount - Step * Convert.ToInt32(tickcount / Step) == 0))
+                {
+                    // Итерационный цикл внутри выбранного периода
+
+                    var valueTickBuy = 0.0;
+                    var valueTickSell = 0.0;
+                    var valueVolBuy = 0.0;
+                    var valueVolSell = 0.0;
+                    for (var j = i; j < i + Step; j++)
+                    {
+                        var t = sec.GetTrades(j);
+                        valueTickBuy += t[0].Direction.ToString() == "Buy" ? 1 : 0;
+                        valueVolBuy += t[0].Direction.ToString() == "Buy" ? t[0].Quantity : 0;
+                        valueTickSell += t[0].Direction.ToString() == "Sell" ? 1 : 0;
+                        valueVolSell += t[0].Direction.ToString() == "Sell" ? t[0].Quantity : 0;
+                        // Считаем осциллятор
+                    }
+                    values[i + Step - 1] = ((valueTickBuy * valueVolBuy - valueTickSell * valueVolSell) /
+                                            (valueTickBuy * valueVolBuy + valueTickSell * valueVolSell));
+                    // Заполняем предшествующие элементы массива последним значением предыдущего шага
+                    //for (var k = i; k < i + Step - 2; k++)
+                    //{
+                    //    if (i == 0)
+                    //    {
+                    //        values[k] = 0.0;
+                    //    }
+                    //    else
+                    //    {
+                    //        values[k] = values[i + Step - 1];
+                    //    }
+                    //}
+                }
+            }
+            var comp = sec.CompressTo(new Interval(Step*sec.Interval, sec.IntervalBase));
+            var vtoBars = new DataBar[comp.Bars.Count];
+            for (int k = 0; k < comp.Bars.Count; k++)
+            {
+                var open = comp.Bars[k].Open;
+                var close = comp.Bars[k].Close;
+                var high = comp.Bars[k].High;
+                var low = comp.Bars[k].Low;
+                var date = comp.Bars[k].Date;
+                var bar = new DataBar(date, open, high, low, close, 10000*values[(k+1) * Step - 1], values[(k + 1) * Step - 1]);
+                vtoBars[k] = bar;
+            }
+            var vto = comp.CloneAndReplaceBars(vtoBars);
+            return vto;
+        }
+    }
+}
---
+++ TickSpeed/VolTickOscMultSpeed.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace TickSpeed
+{
+    // Объемно-тиковый осциллятор.
+    [HandlerCategory("Arelyt")]
+    [HandlerName("VTOMULTSPEED")]
+    public class VolTickOscMultSpeed : IBar2DoubleHandler
+    {
+        
+        public IList<double> Execute(ISecurity security)
+        {
+            var count = security.Bars.Count;
+            var values = new double[count];
+            
+            for (var i = 0; i < count; i++)
+            {
+                var trades = security.GetTrades(i);
+                var valueTickBuy  = 0.0;
+                var valueTickSell = 0.0;
+                var valueVolBuy   = 0.0;
+                var valueVolSell  = 0.0;
+
+                foreach (var t in trades)
+                {
+                    var trd = t;
+                    valueTickBuy += t.Direction.ToString() == "Buy" ? 1 : 0;
+                    valueVolBuy += t.Direction.ToString() == "Buy" ? trd.Quantity : 0;
+                    valueTickSell += t.Direction.ToString() == "Sell" ? 1 : 0;
+                    valueVolSell += t.Direction.ToString() == "Sell" ? trd.Quantity : 0;
+                }
+                // Считаем осциллятор
+
+                values[i] = ((valueTickBuy  - valueTickSell) / (valueTickBuy + valueTickSell)) *
+                            Math.Abs((valueVolBuy - valueVolSell) / (valueVolBuy + valueVolSell)) * Math.Abs(valueTickBuy - valueTickSell);
+            }
+            return values;
+        }
+    }
+}
