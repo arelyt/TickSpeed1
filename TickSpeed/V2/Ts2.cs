eb83a1eb6c43808f13c8f02f0edcf79c31fe91b9
arelyt <arelyt26@gmail.com>
2017-04-23 10:35:19 +0300

    foreach

--- TickSpeed/V2/Ts2.cs
+++ TickSpeed/V2/Ts2.cs
@@ -91,12 +91,12 @@ namespace TickSpeed.V2
             {
                 double valueTickBuy = 0, valueTickSell = 0, valueVolBuy = 0, valueVolSell = 0;
                 var t = scr.GetTradesPerBar(i, i + in1 - 1);
-                for(var j =0; j < t.Count; i++)
+                foreach(var trades in t)
                 {
-                    valueTickBuy += t[j][0].Direction.ToString() == "Buy" ? 1 : 0;
-                    valueVolBuy += t[j][0].Direction.ToString() == "Buy" ? t[j][0].Quantity : 0;
-                    valueTickSell += t[j][0].Direction.ToString() == "Sell" ? 1 : 0;
-                    valueVolSell += t[j][0].Direction.ToString() == "Sell" ? t[j][0].Quantity : 0;
+                    valueTickBuy += trades[0].Direction.ToString() == "Buy" ? 1 : 0;
+                    valueVolBuy += trades[0].Direction.ToString() == "Buy" ? trades[0].Quantity : 0;
+                    valueTickSell += trades[0].Direction.ToString() == "Sell" ? 1 : 0;
+                    valueVolSell += trades[0].Direction.ToString() == "Sell" ? trades[0].Quantity : 0;
 
                 }
                 values[i] = (valueTickBuy - valueTickSell) / (valueTickBuy + valueTickSell) *
