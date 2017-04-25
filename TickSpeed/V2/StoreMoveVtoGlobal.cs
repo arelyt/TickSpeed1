6480cc35b39dd24a5d364349ce67329934bad3d9
arelyt <arelyt26@gmail.com>
2017-04-25 10:43:20 +0300

    StoreMoveVto

---
+++ TickSpeed/V2/StoreMoveVtoGlobal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace TickSpeed.V2
+{
+    public class StoreMoveVtoGlobal : IExternalScript
+    {
+        public int Step = 16;
+        public void Execute(IContext ctx, ISecurity sec)
+        {
+
+            if (sec.IntervalBase.ToString() != "TICK" || sec.Interval.ToString() != "1")
+                throw new Exception("Base Interval wrong. Please set to Tick 1");
+
+
+            double[] vto16 = vto_step(sec, ctx, Step);
+
+
+            //var cache = ctx.LoadGlobalObject("TickPrice");
+            //var price = new double[ctx.BarsCount];
+            //for (var i = 0; i < ctx.BarsCount; i++)
+            //{
+            //   //var t = sec.GetTrades(i);
+            //    price[i] = sec.GetTrades(i)[0].Price;
+            //}
+            
+            ctx.StoreGlobalObject("VTO16", vto16);    
+
+        }
+        public double[] vto_step(ISecurity scr, IContext ctx, int in1)
+        {
+            var count = Convert.ToInt32(ctx.BarsCount / in1);
+            var values = new double[count];
+            var j = 0;
+            for (var i = Step; i < count - Step -1 ; i++)
+            {
+                double valueTickBuy = 0, valueTickSell = 0, valueVolBuy = 0, valueVolSell = 0;
+                var t = scr.GetTradesPerBar(i-Step, i-1);
+                
+                foreach (var trades in t)
+                {
+                    valueTickBuy += trades[0].Direction.ToString() == "Buy" ? 1 : 0;
+                    valueVolBuy += trades[0].Direction.ToString() == "Buy" ? trades[0].Quantity : 0;
+                    valueTickSell += trades[0].Direction.ToString() == "Sell" ? 1 : 0;
+                    valueVolSell += trades[0].Direction.ToString() == "Sell" ? trades[0].Quantity : 0;
+
+                }
+
+                //values[j] = (valueTickBuy - valueTickSell) / (valueTickBuy + valueTickSell) *
+                //            (valueVolBuy - valueVolSell) / (valueVolBuy + valueVolSell);
+
+                values[j] = (valueTickBuy * valueVolBuy - valueTickSell * valueVolSell) /
+                            (valueTickBuy * valueVolBuy + valueTickSell * valueVolSell);
+
+                //values[j] = ((valueTickBuy - valueTickSell) / (valueTickBuy + valueTickSell)) *
+                //           Math.Abs((valueVolBuy - valueVolSell) / (valueVolBuy + valueVolSell));
+            
+                j++;
+            }
+            return values;
+        }
+    }
+}
