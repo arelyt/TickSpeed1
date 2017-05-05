2a8447747ce81ebbf5e6145e1ebe647122f915f5
arelyt <arelyt26@gmail.com>
2017-05-05 11:26:27 +0300

    sqs

--- TickSpeed/StoreGlobalBoolSec.cs
+++ TickSpeed/StoreGlobalBoolSec.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using TSLab.Script;
 using TSLab.Script.Handlers;
 
 namespace TickSpeed
@@ -14,25 +16,30 @@ namespace TickSpeed
     [HandlerName("StoreBool")]
 #pragma warning restore 612
 
-    public class StoreGlobalBoolSec : IDouble2DoubleHandler, IContextUses
+    public class StoreGlobalBoolSec : IContextUses, IOneSourceHandler
     {
         public IContext Context { get; set; }
-        public IList<double> Execute(IList<double> myD)
+        public IList<double> Execute(ISecurity sec, IList<double> myD, IList<double> myD1)
         {
+            var header = sec.Symbol + sec.IntervalBase + sec.Interval;
             var ctx = Context;
             var count = myD.Count;
+            var count1 = myD1.Count;
             var values = new double[count];
-            if (count == 0)
+            if (count == 0 || count != count1)
             {
                 return values;
             }
-            var signalBuy = myD[count-1] > myD[count - 2] && myD[count - 2] < myD[count - 3];
-            var signalSell = myD[count - 1] < myD[count - 2] && myD[count - 2] > myD[count - 3];
-            ctx.StoreGlobalObject("SiM7_Buy", signalBuy);
-            ctx.StoreGlobalObject("SiM7_Sell", signalSell);
+            var signalBuySWT = myD[count-1] > myD[count - 2] && myD[count - 2] < myD[count - 3];
+            var signalSellSWT = myD[count - 1] < myD[count - 2] && myD[count - 2] > myD[count - 3];
+            var signalBuyTSpeed = myD1[count1] < 0 && myD1[count1 - 1] > 0;
+            var signalSellTSpeed = myD1[count1] > 0 && myD1[count1 - 1] < 0;
+            ctx.StoreGlobalObject(header + "SWT_Buy", signalBuySWT);
+            ctx.StoreGlobalObject(header + "SWT_Sell", signalSellSWT);
+            ctx.StoreGlobalObject(header + "TSpeed_Buy", signalBuyTSpeed);
+            ctx.StoreGlobalObject(header + "TSpeed_Sell", signalSellTSpeed);
             return values;
         }
-
 
         
     }
