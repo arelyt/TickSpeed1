1a71b126cc2c8d93abd4bae66c2bb57b57a08ccf
arelyt <arelyt26@gmail.com>
2017-05-31 15:09:25 +0300

    TSO

---
+++ TickSpeed/TickSpeedOsc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace TickSpeed
+{
+    // Объемно-тиковый осциллятор.
+    [HandlerCategory("Arelyt")]
+#pragma warning disable 612
+    [HandlerName("TSO")]
+#pragma warning restore 612
+    public class TickSpeedOsc : IBar2DoubleHandler
+    {
+        
+        public IList<double> Execute(ISecurity security)
+        {
+            var count = security.Bars.Count;
+            var values = new double[count];
+            var datme = new double[count];
+            if (count < 2)
+                return null;
+
+
+            for (var i = 0; i < count; i++)
+            {
+                var trades = security.GetTrades(i);
+                var valueTickBuy  = 0.0;
+                var valueTickSell = 0.0;
+                datme[i] = TimeSpan.FromTicks(security.Bars[i].Date.Ticks - security.Bars[i - 1].Date.Ticks).TotalSeconds;
+                foreach (var t in trades)
+                {
+                    var trd = t;
+                    valueTickBuy += t.Direction.ToString() == "Buy" ? 1 : 0;
+                    valueTickSell += t.Direction.ToString() == "Sell" ? 1 : 0;
+                    
+                }
+                // Считаем осциллятор
+
+                values[i] = Math.Tanh((valueTickBuy - valueTickSell) /
+                                 (valueTickBuy + valueTickSell));
+            }
+            return values;
+        }
+    }
+}
