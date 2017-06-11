1d32912a640fb9a007f206e4edb9463096ca6b7d
arelyt <arelyt26@gmail.com>
2017-06-11 11:12:13 +0300

    vtointerp

---
+++ TickSpeed/V2/TestCounterTicks.cs
@@ -0,0 +1,28 @@
+﻿using RusAlgo.Helper;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace TickSpeed.V2
+{
+    public class TestCounterTicksClass : IExternalScript
+    {
+        public static double Counter;
+        public void Execute(IContext ctx, ISecurity sec)
+        {
+            
+            //if (sec.IntervalBase.ToString() != "TICK" || sec.Interval.ToString() != "1")
+            //    throw new Exception("Base Interval wrong. Please set to Tick 1");
+            Counter++;
+            //var cache = ctx.LoadGlobalObject("TickPrice");
+            //var price = new double[ctx.BarsCount];
+            //for (var i = 0; i < ctx.BarsCount; i++)
+            //{
+            //   //var t = sec.GetTrades(i);
+            //    price[i] = sec.GetTrades(i)[0].Price;
+            //}
+            
+            //ctx.StoreGlobalObject("TickPrice", price);    
+            ctx.Log("Counter {0}".Put(Counter), MessageType.Info, true);
+        }
+    }
+}
---
+++ TickSpeed/VtoInterpWin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using TSLab.Script.Handlers;
+using MathWorks.MATLAB.ProductionServer.Client;
+using TSLab.Script;
+
+namespace TickSpeed
+{
+    // Полный осциллятор потока + произвольный
+    // тиковый таймфрейм + вейвлет-шумоподавление +
+    // интерполяция сигнала по тиковому таймфрейму
+    [HandlerCategory("Arelyt")]
+#pragma warning disable 612
+    [HandlerName("VTOInterpol")]
+#pragma warning restore 612
+    public class VtoInterpWinClass : IBar2DoubleHandler
+    {
+        public interface IVtoInterpol
+        {
+            double[] vtointerp_auto(double[] in1, double[] in2, double in3);
+        }
+        //[HandlerParameter(Name = "Order", NotOptimized = false)]
+        //public double Order { get; set; }
+        [HandlerParameter(Name = "Win", NotOptimized = false)]
+        public double Win { get; set; }
+
+        public IList<double> Execute(ISecurity security)
+        {
+            if (security.IntervalBase.ToString() != "TICK" || security.Interval.ToString() != "1")
+                throw new Exception("Base Interval wrong. Please set to Tick 1");
+            var count = security.Bars.Count;
+            if (count < 2)
+                return null;
+            var result = new double[count];
+            var values1 = new double[count];
+            var values2 = new double[count];
+            for (var i = 0; i < count; i++)
+            {
+                var trades = security.GetTrades(i);
+                foreach (var t in trades)
+                {
+                    var trd = t;
+                    values1[i] += t.Direction.ToString() == "Buy" ? trd.Quantity : 0;
+                    values2[i] += t.Direction.ToString() == "Sell" ? trd.Quantity : 0;
+                }
+            }
+            // Начинаем wden + interpolation process
+
+
+            MWClient client = new MWHttpClient();
+            try
+            {
+                IVtoInterpol sigDen = client.CreateProxy<IVtoInterpol>(new Uri("http://localhost:9910/vtointerp_auto_dep"));
+                result = sigDen.vtointerp_auto(values1, values2, Win);
+            }
+            catch (MATLABException)
+            {
+
+            }
+            finally
+            {
+                client.Dispose();
+            }
+            return result;
+            
+        }
+    }
+}
