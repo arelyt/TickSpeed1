6b5e18db8a0ec8f5b96eba50d91a71d9c0d55a08
arelyt <arelyt26@gmail.com>
2017-07-07 21:19:55 +0300

    ssa

---
+++ TickSpeed/Ssa.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using TSLab.Script.Handlers;
+using MathWorks.MATLAB.ProductionServer.Client;
+
+namespace TickSpeed
+{
+    // Объемно-тиковый осциллятор.
+    [HandlerCategory("Arelyt")]
+#pragma warning disable 612
+    [HandlerName("SSA")]
+#pragma warning restore 612
+    public class SsaClass : IDouble2DoubleHandler
+    {
+        public interface ISsaFor
+        {
+            double[] Ssa(double[] in1, int in2, int in3, int in4);
+        }
+
+        [HandlerParameter(true, "60", Name = "Win", Max = "1000", Min = "1", Step = "1", NotOptimized = false)]
+        public int Numdec { get; set; }
+
+        [HandlerParameter(true, "5", Name = "NumCompRec", Max = "10", Min = "1", Step = "1", NotOptimized = false)]
+        public int Numrec { get; set; }
+
+        [HandlerParameter(true, "1", Name = "NumForForecast", Max = "10", Min = "1", Step = "1", NotOptimized = false)]
+        public int Numfor { get; set; }
+
+        public IList<double> Execute(IList<double> myDoubles)
+        {
+            var count = myDoubles.Count;
+            if (count < 2)
+                return null;
+            var result = new double[count];
+            var values = new double[count];
+            for (var i = 0; i < count; i++)
+            {
+                values[i] = myDoubles[i];
+            }
+            // Начинаем Signal denoising process
+
+            // Wavelet DB3 Level 5
+            MWClient client = new MWHttpClient();
+            try
+            {
+                ISsaFor sigDen = client.CreateProxy<ISsaFor>(new Uri("http://localhost:9910/Ssa_dep"));
+                result = sigDen.Ssa(values, Numdec, Numrec, Numfor);
+            }
+            catch (MATLABException)
+            {
+
+            }
+            finally
+            {
+                client.Dispose();
+            }
+            return result;
+        }
+    }
+}
