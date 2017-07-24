df53e7ef95cfcc1dab0aa37ab6fbf71487266dd6
ROMAN DENISOV <arelyt26@gmail.com>
2017-07-24 12:29:36 +0300

    rbfAlgoLib

---
+++ TickSpeed/RbfSmoothAlgLib.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+using static alglib;
+
+namespace TickSpeed
+{
+    // RBF model from AlgoLib.
+    [HandlerCategory("Arelyt")]
+#pragma warning disable 612
+    [HandlerName("RBFSmoothAlgLib")]
+#pragma warning restore 612
+    public class RbfSmoothAlgLibClass : IBar2DoubleHandler
+    {
+        [HandlerParameter(Name = "NLayer", Default = "3", NotOptimized = false)]
+        public int Nlayer { get; set; }
+        [HandlerParameter(Name = "Smooth", Default = "0.01", NotOptimized = false)]
+        public double Smooth { get; set; }
+        [HandlerParameter(Name = "RbfConst", Default = "2.0", NotOptimized = false)]
+        public double Rbfconst { get; set; }
+
+        private rbfmodel _model;
+        
+
+        
+        public IList<double> Execute(ISecurity security)
+        {
+            var count = security.Bars.Count;
+            if (count < 10)
+                return null;
+            rbfcreate(2, 1, out _model);
+            // var v = alglib.rbfcalc2(model, 0.0, 0.0);
+            var result = new double[count];
+            var values = new double[count];
+            var time = new double[count];
+            for (var i = 0; i < count; i++)
+            {
+                values[i] = security.Bars[i].Close;
+                time[i] = i;
+            }
+            var xy = new double[count, 3];
+            for (var i = 0; i < count; i++)
+            {
+                xy[i, 0] = time[i];
+                xy[i, 2] = values[i];
+            }
+            rbfsetpoints(_model, xy);
+            // v = alglib.rbfcalc2(model, 0.0, 0.0);
+            rbfreport rep;
+            rbfsetalgomultilayer(_model, Rbfconst, Nlayer, Smooth);
+            rbfbuildmodel(_model, out rep);
+            for (var i = 0; i < count; i++)
+            {
+                result[i] = rbfcalc2(_model, time[i], 0.0);
+            }
+            return result;
+        }
+    }
+}
---
+++ rbftest/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace rbftest
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            //
+            // This example illustrates basic concepts of the RBF models: creation, modification,
+            // evaluation.
+            // 
+            // Suppose that we have set of 2-dimensional points with associated
+            // scalar function values, and we want to build a RBF model using
+            // our data.
+            // 
+            // NOTE: we can work with 3D models too :)
+            // 
+            // Typical sequence of steps is given below:
+            // 1. we create RBF model object
+            // 2. we attach our dataset to the RBF model and tune algorithm settings
+            // 3. we rebuild RBF model using QNN algorithm on new data
+            // 4. we use RBF model (evaluate, serialize, etc.)
+            //
+
+            //
+            // Step 1: RBF model creation.
+            //
+            // We have to specify dimensionality of the space (2 or 3) and
+            // dimensionality of the function (scalar or vector).
+            //
+            // New model is empty - it can be evaluated,
+            // but we just get zero value at any point.
+            //
+            alglib.rbfmodel model;
+            alglib.rbfcreate(2, 1, out model);
+
+            var v = alglib.rbfcalc2(model, 0.0, 0.0);
+            System.Console.WriteLine("{0:F2}", v); // EXPECTED: 0.000
+
+            //
+            // Step 2: we add dataset.
+            //
+            // XY contains two points - x0=(-1,0) and x1=(+1,0) -
+            // and two function values f(x0)=2, f(x1)=3.
+            //
+            // We added points, but model was not rebuild yet.
+            // If we call rbfcalc2(), we still will get 0.0 as result.
+            //
+            double[,] xy = new double[,] { { -1, 0, 2 }, { +1, 0, 3 } };
+            alglib.rbfsetpoints(model, xy);
+            System.Console.WriteLine("{0:F2}", xy);
+            System.Console.ReadLine();
+
+            v = alglib.rbfcalc2(model, 0.0, 0.0);
+            System.Console.WriteLine("{0:F2}", v); // EXPECTED: 0.000
+
+            //
+            // Step 3: rebuild model
+            //
+            // After we've configured model, we should rebuild it -
+            // it will change coefficients stored internally in the
+            // rbfmodel structure.
+            //
+            // We use hierarchical RBF algorithm with following parameters:
+            // * RBase - set to 1.0
+            // * NLayers - three layers are used (although such simple problem
+            //   does not need more than 1 layer)
+            // * LambdaReg - is set to zero value, no smoothing is required
+            //
+            alglib.rbfreport rep;
+            alglib.rbfsetalgomultilayer(model, 1.0, 3, 0.0);
+            alglib.rbfbuildmodel(model, out rep);
+            System.Console.WriteLine("{0}", rep.terminationtype); // EXPECTED: 1
+
+            //
+            // Step 4: model was built
+            //
+            // After call of rbfbuildmodel(), rbfcalc2() will return
+            // value of the new model.
+            //
+            v = alglib.rbfcalc2(model, 0.0, 0.0);
+            System.Console.WriteLine("{0:F2}", v); // EXPECTED: 2.500
+            System.Console.ReadLine();
+        }
+    }
+}
