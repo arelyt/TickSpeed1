c8af386cfe625ca39388351db966d435a7d32e77
arelyt <arelyt26@gmail.com>
2017-08-25 22:28:13 +0300

    yty

---
+++ TickSpeed/CasheSignalDouble1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+using RusAlgo.Helper;
+using TSLab.Script.Handlers;
+using TSLab.Script;
+using TSLab.Utils;
+
+namespace TickSpeed
+{
+    // Cashe bool signal
+    [HandlerCategory("Arelyt")]
+#pragma warning disable 612
+    [HandlerName("CasheSignalDouble1")]
+#pragma warning restore 612
+    public class CasheSignalDouble1Class : ITwoSourcesHandler, ISecurityInput0, IDoubleInput1, IStreamHandler, IDoubleReturns
+    {
+        
+        [HandlerParameter(Name = "Values", Default = "true", NotOptimized = false)]
+        public bool Reset { get; set; }
+        private static IList<double> Tradecashe1 { get; set; }
+        private static IList<double> Doublecashe1 { get; set; }
+        //public static IList<double> Ncashe { get; set; }
+
+        public IList<double> Execute(ISecurity sec, IList<double> bools)
+        {
+            var count = sec.Bars.Count;
+            if (count < 100)
+                return null;
+            //var result = new double[count];
+            //var price = new double[count];
+            var tradeno = new double[count];
+            //var time = new double[count];
+            
+            for (var i = 0; i < count; i++)
+            {
+                tradeno[i] = sec.Bars[i].FirstTradeId.Number;
+                //price[i] = sec.Bars[i].Close;
+                //time[i] = sec.Bars[i].Date.TimeOfDay.TotalSeconds;
+                
+            }
+            if (Doublecashe1.IsNull() || Doublecashe1.IsNull() || Reset)
+            {
+                
+                Tradecashe1 = tradeno.ToList();
+                Doublecashe1 = bools.ToList();
+                //Tcashe = time.ToList();
+            }
+            else
+            {
+                var s = Tradecashe1.Last();
+                var delta =count - Array.FindIndex(tradeno, 0, w => w.Equals(s)) - 1;
+
+                
+                var bl = bools.Skip(count - delta).Take(delta).ToList();
+                Doublecashe1.AddRange(bl);
+                var tr = tradeno.Skip(count - delta).Take(delta).ToList();
+                Tradecashe1.AddRange(tr);
+                //var ti = time.Skip(count - delta).Take(delta).ToList();
+                //Tcashe.AddRange(ti);
+                
+            }
+
+            return Doublecashe1.TakeLast(count).ToArray();          
+
+        }
+        
+    }
+}
--- TickSpeed/SsaAdap.cs
+++ TickSpeed/SsaAdap.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using TSLab.Script.Handlers;
 using MathWorks.MATLAB.ProductionServer.Client;
 // ReSharper disable InconsistentNaming
@@ -10,9 +11,9 @@ namespace TickSpeed
     // Объемно-тиковый осциллятор.
     [HandlerCategory("Arelyt")]
 #pragma warning disable 612
-    [HandlerName("SSA")]
+    [HandlerName("SSAAdap")]
 #pragma warning restore 612
-    public class SsaClass : IOneSourceHandler, IDoubleInputs, IDoubleReturns, IStreamHandler, IContextUses
+    public class SsaAdapClass : ITwoSourcesHandler, IDoubleInput0, IDoubleInput1, IDoubleReturns, IStreamHandler, IContextUses
     {
         public IContext Context { get; set; }
         public interface ISsaFor
@@ -22,8 +23,8 @@ namespace TickSpeed
 
         }
 
-        [HandlerParameter(true, "60", Name = "Win", Max = "1000", Min = "1", Step = "1", NotOptimized = false)]
-        public int Numdec { get; set; }
+        //[HandlerParameter(true, "60", Name = "Win", Max = "1000", Min = "1", Step = "1", NotOptimized = false)]
+        //public int Numdec { get; set; }
 
         [HandlerParameter(true, "5", Name = "NumCompRec", Max = "10", Min = "1", Step = "1", NotOptimized = false)]
         public int Numrec { get; set; }
@@ -31,9 +32,10 @@ namespace TickSpeed
         //[HandlerParameter(true, "1", Name = "NumForForecast", Max = "10", Min = "1", Step = "1", NotOptimized = false)]
         //public int Numfor { get; set; }
 
-        public IList<double> Execute(IList<double> myDoubles)
+        public IList<double> Execute(IList<double> myDoubles, IList<double> sp)
         {
             var count = myDoubles.Count;
+            var speed = (int) Math.Floor(sp.Last() * 10);
             if (count < 2)
                 return null;
             var result = new double[count];
@@ -50,7 +52,7 @@ namespace TickSpeed
             try
             {
                 ISsaFor sigDen = client.CreateProxy<ISsaFor>(new Uri("http://localhost:9910/ssarec_dep"));
-                result = sigDen.ssarec(values, Numdec, Numrec);
+                result = sigDen.ssarec(values, speed, Numrec);
             }
             catch (MATLABException)
             {
@@ -61,7 +63,7 @@ namespace TickSpeed
                 client.Dispose();
             }
             var g = (DateTime.Now - t).TotalMilliseconds.ToString(CultureInfo.InvariantCulture);
-            Context.Log("ssa exec for "+g+" msec", MessageType.Info, toMessageWindow:true);
+            Context.Log("ssaadap exec for "+g+" msec", MessageType.Info, toMessageWindow:true);
             return result;
         }
 
