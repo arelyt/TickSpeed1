cc82e18c4ff9714b32928ecb4e173282c8af1b89
arelyt <arelyt26@gmail.com>
2017-09-14 15:25:02 +0300

    gosc

---
+++ TickSpeed/IbyGlassOsc.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+//using System.Linq;
+//using MoreLinq;
+//using RusAlgo.Helper;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace TickSpeed
+{
+
+    [HandlerCategory("Arelyt")]
+#pragma warning disable 612
+    [HandlerName("IbyGlassOsc")]
+#pragma warning restore 612
+    public class IbyGlassOscClass : ISecurityInputs, IDoubleReturns, IOneSourceHandler, IStreamHandler
+    {
+        [HandlerParameter(true, "0", Min = "1", Max = "20", Step = "1")]
+        public int Level { get; set; }
+        [HandlerParameter(Name = "Reset", Default = "true", NotOptimized = false)]
+        public bool Reset { get; set; }
+        //private static IList<double> Glass { get; set; }
+        public IList<double> Execute(ISecurity source)
+        {
+            var count = source.Bars.Count;
+            var maxlev = source.GetSellQueue(0).Count;
+            var numArray = new double[source.Bars.Count];
+            if (count < 10 || maxlev == 0)
+                return null;
+            //if (TradeHelper.IsNull(Glass) || Reset)
+            //{
+            //    var numarray = new double[count];
+            //    Glass = numarray.ToList();
+            //    var nSell = 0.0;
+            //    var nBuy = 0.0;
+            //    var sellQueue = source.GetSellQueue(0);
+            //    var buyQueue = source.GetBuyQueue(0);
+            //    for (var i = 0; i < maxlev; i++)
+            //    {
+            //        nSell += sellQueue[i].Quantity;
+            //        nBuy += buyQueue[i].Quantity;
+            //    }
+            //    Glass[count] = nBuy/(nBuy + nSell) -1;
+
+            //}
+            //else
+            //{
+            //    var nSell = 0.0;
+            //    var nBuy = 0.0;
+            //    var sellQueue = source.GetSellQueue(0);
+            //    var buyQueue = source.GetBuyQueue(0);
+            //    for (var i = 0; i < maxlev; i++)
+            //    {
+            //        nSell += sellQueue[i].Quantity;
+            //        nBuy += buyQueue[i].Quantity;
+            //    }
+            //    Glass.Add(nBuy / (nBuy + nSell) - 1);
+            //}
+
+            //return Glass.TakeLast(count).ToArray();
+            for (int i = 0; i < count; i++)
+            {
+                var nSell = 0.0;
+                var nBuy = 0.0;
+                var sellQueue = source.GetSellQueue(0);
+                var buyQueue = source.GetBuyQueue(0);
+                for (var j = 0; j < Level; j++)
+                {
+                    nSell += sellQueue[j].Quantity;
+                    nBuy += buyQueue[j].Quantity;
+                }
+                numArray[i] = (nBuy - nSell) / (nBuy + nSell);
+            }
+            return numArray;
+        }
+        
+    }
+}
+
