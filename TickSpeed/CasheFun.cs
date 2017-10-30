b56a11f8aa72d60df1a42e98ee43ffed2df0234b
ROMAN DENISOV <arelyt26@gmail.com>
2017-10-30 13:18:17 +0300

    cashefun1

---
+++ TickSpeed/CasheFun.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using TSLab.Script.Handlers;
+using MathWorks.MATLAB.ProductionServer.Client;
+using TSLab.Script;
+
+namespace TickSpeed
+{
+    // Объемно-тиковый осциллятор.
+    [HandlerCategory("Arelyt")]
+#pragma warning disable 612
+    [HandlerName("CasheFun")]
+#pragma warning restore 612
+    public class CasheFunClass : IThreeSourcesHandler, ISecurityInput0,
+                                 IDoubleInput1, IDoubleInput2, IDoubleReturns,
+                                 IContextUses
+    {
+       
+        
+        public interface ICasheFun1
+        {
+            // ReSharper disable once InconsistentNaming
+            double[] cashefun1(double[] in1, double[] in2, double[] in3);
+        }
+        //[HandlerParameter(Name = "Values", NotOptimized = true)]
+        //public V2.Predin Line { get; set; }
+
+        public IList<double> Execute(ISecurity sec, IList<double> halfspread, IList<double> glassosc)
+        {
+            var count = sec.Bars.Count;
+            if (count < 100)
+                return null;
+            var result = new double[count];
+            var price = new double[count];
+            var tradeno = new double[count];
+            //var askq = new double[count];
+            //var bidq = new double[count];
+            //var askp = new double[count];
+            //var bidp = new double[count];
+            var time = new double[count];
+            for (var i = 0; i < count; i++)
+            {
+                price[i] = halfspread[i];
+                
+                tradeno[i] = sec.Bars[i].FirstTradeId.Number;
+                time[i] = sec.Bars[i].Date.TimeOfDay.TotalMilliseconds - 36000;
+            }
+            
+            MWClient client = new MWHttpClient();
+            try
+            {
+                ICasheFun1 sigDen = client.CreateProxy<ICasheFun1>(new Uri("http://localhost:9910/cashefun1_dep"));
+                result = sigDen.cashefun1(time, tradeno, price);
+            }
+            catch (MATLABException)
+            {
+
+            }
+            finally
+            {
+                client.Dispose();
+            }
+            return result;
+        }
+
+
+        public IContext Context { get; set; }
+    }
+}
--- TickSpeed/Detrend.cs
+++ TickSpeed/Detrend.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using MathWorks.MATLAB.ProductionServer.Client;
 using TSLab.Script.Handlers;
 
@@ -15,30 +16,31 @@ namespace TickSpeed
         public interface IDetrend
         {
             // ReSharper disable once InconsistentNaming
-            double[] detrend1(double[] in1, double[] in2);
+            double[] detrend1(double[] in1, double in2);
         }
         [HandlerParameter(Name = "Step", NotOptimized = false)]
         public double Step { get; set; }
+
         public IList<double> Execute(IList<double> myDoubles)
         {
             var count = myDoubles.Count;
             if (count < 2)
                 return null;
             var values = new double[count];
-            var l = (int)Math.Floor(count / Step);
-            var vector = new double[l];
-            vector[0] = 0;
-            for (var i = 1; i < l; i++)
-            {
-                vector[i] = vector[i-1] + Step;
-            }
+            //var l = (int)Math.Floor(count / Step);
+            //var vector = new double[l];
+            //vector[0] = 0;
+            //for (var i = 1; i < l; i++)
+            //{
+            //    vector[i] = vector[i-1] + Step;
+            //}
 
 
             MWClient client = new MWHttpClient();
             try
             {
                 IDetrend sigDen = client.CreateProxy<IDetrend>(new Uri("http://localhost:9910/detrend1_dep"));
-                values = sigDen.detrend1(values, vector);
+                values = sigDen.detrend1(myDoubles.ToArray(), Step);
             }
             catch (MATLABException)
             {
