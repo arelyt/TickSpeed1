6fec7e09e99ab7d2ba1dc46b752705f23eae1e46
ROMAN DENISOV <arelyt26@gmail.com>
2017-10-31 16:11:24 +0300

    vcvc

--- TickSpeed/CasheFun.cs
+++ TickSpeed/CasheFun.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using TSLab.Script.Handlers;
 using MathWorks.MATLAB.ProductionServer.Client;
+using TSLab.DataSource;
 using TSLab.Script;
+using TSLab.Script.Helpers;
 
 namespace TickSpeed
 {
@@ -12,19 +15,17 @@ namespace TickSpeed
     [HandlerName("CasheFun")]
 #pragma warning restore 612
     public class CasheFunClass : IThreeSourcesHandler, ISecurityInput0,
-                                 IDoubleInput1, IDoubleInput2, IDoubleReturns,
-                                 IContextUses
+                                 IDoubleInput1, IDoubleInput2, IDoubleReturns
     {
-       
-        
         public interface ICasheFun1
         {
             // ReSharper disable once InconsistentNaming
-            double[] cashefun1(double[] in1, double[] in2, double[] in3);
+            double[] cashefun1(double[] in1, double[] in2, double[] in3, double[] in4, double[] in5);
         }
         //[HandlerParameter(Name = "Values", NotOptimized = true)]
         //public V2.Predin Line { get; set; }
-
+        [HandlerParameter(Name = "Win", NotOptimized = false)]
+        public int Win { get; set; }
         public IList<double> Execute(ISecurity sec, IList<double> halfspread, IList<double> glassosc)
         {
             var count = sec.Bars.Count;
@@ -33,6 +34,8 @@ namespace TickSpeed
             var result = new double[count];
             var price = new double[count];
             var tradeno = new double[count];
+            IList<double> nBuy = new double[count];
+            IList<double> nSell = new double[count];
             //var askq = new double[count];
             //var bidq = new double[count];
             //var askp = new double[count];
@@ -45,12 +48,32 @@ namespace TickSpeed
                 tradeno[i] = sec.Bars[i].FirstTradeId.Number;
                 time[i] = sec.Bars[i].Date.TimeOfDay.TotalMilliseconds - 36000;
             }
-            
+
+            for (var i = Win - 1; i < count; i++)
+            {
+                var tradelastwin = sec.GetTrades(firstBarIndex: i - Win + 1, lastBarIndex: i);
+                nBuy[i] = tradelastwin.Sum(selector: t => t.Direction == TradeDirection.Buy ? 1 : 0) / (double)Win;
+                nSell[i] = tradelastwin.Sum(selector: t => t.Direction == TradeDirection.Sell ? 1 : 0) / (double)Win;
+            }
+            nBuy = Series.EMA(nBuy, Win);
+            nSell = Series.EMA(nSell, Win);
+            var osc = new double[count];
+            for (int i = 0; i < count; i++)
+            {
+                if (Math.Abs(nSell[i]) > 0.0001)
+                {
+                    osc[i] = nBuy[i] / nSell[i];
+                }
+                else
+                {
+                    osc[i] = 0.0;
+                }
+            }
             MWClient client = new MWHttpClient();
             try
             {
                 ICasheFun1 sigDen = client.CreateProxy<ICasheFun1>(new Uri("http://localhost:9910/cashefun1_dep"));
-                result = sigDen.cashefun1(time, tradeno, price);
+                result = sigDen.cashefun1(time, tradeno, price, osc, glassosc.ToArray());
             }
             catch (MATLABException)
             {
@@ -62,8 +85,6 @@ namespace TickSpeed
             }
             return result;
         }
-
-
-        public IContext Context { get; set; }
+        
     }
 }
---
+++ TickSpeed/Znorm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathWorks.MATLAB.ProductionServer.Client;
+using TSLab.Script.Handlers;
+
+namespace TickSpeed
+{
+    // Скользящее нормализованное окно накопительной дельты
+#pragma warning disable 612
+    [HandlerCategory("Arelyt")]
+    [HandlerName("ZNorm")]
+#pragma warning restore 612
+    public class ZNormClass : IDouble2DoubleHandler
+    {
+        public interface IZnorm
+        {
+            double[] znorm(double[] in1, double in2);
+        }
+        [HandlerParameter(true, "64", Name = "Win")]
+        public int Win { get; set; }
+
+        //[HandlerParameter(true, "0.5", Name = "K")]
+        //public double K { get; set; }
+
+        public IList<double> Execute(IList<double> myDoubles)
+        {
+            
+            var count = myDoubles.Count;
+            if (count < 2)
+                return null;
+            var values = new double[count]; // values result
+           
+            MWClient client = new MWHttpClient();
+            try
+            {
+                IZnorm sigDen = client.CreateProxy<IZnorm>(new Uri("http://localhost:9910/znorm_dep"));
+                values = sigDen.znorm(myDoubles.ToArray(), Win);
+            }
+            catch (MATLABException)
+            {
+
+            }
+            finally
+            {
+                client.Dispose();
+            }
+            return values;
+        }
+    }
+}
