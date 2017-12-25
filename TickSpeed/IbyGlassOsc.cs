6576d21f1e5444fccdadcc9b3190fcc324e917f2
ROMAN DENISOV <arelyt26@gmail.com>
2017-12-25 16:02:37 +0300

    gfdgdf

--- TickSpeed/IbyGlassOsc.cs
+++ TickSpeed/IbyGlassOsc.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using RusAlgo.Helper;
 //using System.Linq;
 //using MoreLinq;
 //using RusAlgo.Helper;
@@ -12,20 +14,33 @@ namespace TickSpeed
 #pragma warning disable 612
     [HandlerName("IbyGlassOsc")]
 #pragma warning restore 612
-    public class IbyGlassOscClass : ISecurityInputs, IDoubleReturns, IOneSourceHandler, IStreamHandler
+    public class IbyGlassOscClass : ISecurityInputs, IDoubleReturns, IOneSourceHandler, IStreamHandler, IContextUses
     {
         [HandlerParameter(true, "0", Min = "1", Max = "20", Step = "1")]
         public int Level { get; set; }
         [HandlerParameter(Name = "Reverse", Default = "true", NotOptimized = false)]
         public bool Reverse { get; set; }
+        [HandlerParameter(true, "glass", Name = "ObjName", NotOptimized = false)]
+        public string Objname { get; set; }
+        public IContext Context { get; set; }
+
+        public IList<double> glass;
         //private static IList<double> Glass { get; set; }
         public IList<double> Execute(ISecurity source)
         {
+            var ctx = Context;
             var count = source.Bars.Count;
             var maxlev = source.GetSellQueue(0).Count;
             var numArray = new double[source.Bars.Count];
             if (count < 10 || maxlev == 0)
                 return null;
+            if (glass.IsNull())
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    glass[i] = 0.0;
+                }
+            }
             //if (TradeHelper.IsNull(Glass) || Reset)
             //{
             //    var numarray = new double[count];
@@ -78,7 +93,9 @@ namespace TickSpeed
                 }
                 
             }
-            return numArray;
+            glass.Add(numArray.Last());
+            ctx.StoreGlobalObject(Objname, glass);
+            return glass;
         }
         
     }
---
+++ TickSpeed/LoadGlobalObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MoreLinq;
+using TSLab.Script;
+using TSLab.Script.Handlers;
+
+namespace TickSpeed
+{
+    // Считываем из локального кэша
+    [HandlerCategory("Arelyt")]
+#pragma warning disable 612
+    [HandlerName("LoadGlobalDouble")]
+#pragma warning restore 612
+
+    public class LoadGlobalObjectClass : IOneSourceHandler, ISecurityInputs,
+                                        IDoubleReturns, IStreamHandler, IContextUses 
+    { 
+        public IContext Context { get; set; }
+        [HandlerParameter(true, "fore", Name = "ObjName", NotOptimized = false)]
+        public string Objname { get; set; }
+
+        //[HandlerParameter(true, "5", Name = "Order", Max = "10", Min = "1", Step = "1", NotOptimized = false)]
+        //public int Order { get; set; }
+
+        //[HandlerParameter(true, "61", Name = "winSG", Max = "120", Min = "1", Step = "1", NotOptimized = false)]
+        //public int WinSg { get; set; }
+
+        //[HandlerParameter(true, "0", Name = "deriv", Max = "3", Min = "0", Step = "1", NotOptimized = false)]
+        //public int Deriv { get; set; }
+
+        //[HandlerParameter(true, "0", Name = "LShift", Max = "20", Min = "0", Step = "1", NotOptimized = false)]
+        //public int ShiftL { get; set; }
+
+        public IList<double> Execute(ISecurity sec)
+        {
+            var ctx = Context;
+            //var values = new double[sec.Bars.Count];
+            var values = (IList<double>)ctx.LoadGlobalObject(Objname);
+            var result = new double[values.Count];
+            //SavitzkyGolay sg = new SavitzkyGolay(WinSg, Deriv, Order);
+            //sg.Apply(values.ToArray(), result);
+            //var result1 = result.TakeLast(values.Count - ShiftL);
+            return values;
+        }
+
+        
+    }
+
+}
