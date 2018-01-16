4ca4e9b8e81b527f3e22f73e04c37a1993e7e455
ROMAN DENISOV <arelyt26@gmail.com>
2018-01-16 12:56:41 +0300

    asdf

--- TickSpeed/DifferStack.cs
+++ TickSpeed/DifferStack.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using MoreLinq;
 using TSLab.Script.Handlers;
-using TSLab.Utils;
 using vvTSLtools;
 
 namespace TickSpeed
@@ -18,6 +16,8 @@ namespace TickSpeed
         public IContext Context {get; set;}
         [HandlerParameter(true, "stack", Name = "ObjName", NotOptimized = false)]
         public string Objname { get; set; }
+        [HandlerParameter(true, Name = "Diff", NotOptimized = false)]
+        public bool Diff { get; set; }
 
         public IList<double> Execute(IList<double> myDoubles)
         {
@@ -27,31 +27,42 @@ namespace TickSpeed
                 return null;
             var values = new double[count];
             values[0] = 0;
-
-            for (var i = 1; i < count; i++)
+            if (Diff)
             {
-                values[i] = myDoubles[i] - myDoubles[i - 1];
-            }
-            double[] _val = (double[])ctx.LoadObject(Objname);
-            if (_val.IsNull())
-            {
-                ctx.StoreObject(Objname, values);
-                _val = values;
+                for (var i = 1; i < count; i++)
+                {
+                    values[i] = myDoubles[i] - myDoubles[i - 1];
+                }
             }
             else
             {
-                var delta = values.Length - _val.Length;
+                for (var i = 1; i < count; i++)
+                {
+                    values[i] = myDoubles[i];
+                }
+            }
+            if (ctx.LoadObject(Objname).IsNull())
+            {
+                ctx.StoreObject(Objname, values);
+                var val = (double[])ctx.LoadObject(Objname);
+                return val.ToList();
+            }
+            else
+            {
+                var val = (double[])ctx.LoadObject(Objname);
+                var delta = values.Length - val.Length;
                 var bl = values.Skip(count - delta).Take(delta).ToList();
-                _val.AddRange(bl);
+                var temp = val.ToList();
+                temp.AddRange(bl);
                 //if (_val.Length != count)
                 //{
                 //    Array.Resize(ref _val, count);
                 //}
                 //Array.Copy(_val, values, _val.Length);
-                ctx.StoreObject(Objname, _val.TakeLast(count));
-                
+                ctx.StoreObject(Objname, temp.TakeLast(count).ToArray());
+                return temp.ToList();
             }
-            return _val.ToList();
+            
         }
         
     }
